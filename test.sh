8a0fb3b2243d198014417ed81a5407a8947722aa
Stromsky99 <446554759@qq.com>
2021-07-26 17:56:12 +0800

    github upload

--- test.sh
+++ test.sh
@@ -1,1 +1,16 @@
-test
+git init
+git remote set-url origin https://github.com/Stormsky99/vscode.git
+
+git config --global user.name "Stromsky99"
+git config --global user.email "446554759@qq.com"
+
+git remote add origin https://github.com/Stormsky99/vscode.git
+
+git add test.sh
+
+git commit -m "test"
+
+git show-ref
+
+git branch -M main
+git push -u origin master
